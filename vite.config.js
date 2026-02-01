442367b3d60a8fd6bcfe5c9570bd4b957df59990
Amit Dhangar <amitdhangar531@gmail.com>
2026-02-01 16:30:23 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// https://vite.dev/config/
+
 export default defineConfig({
+  base: '/', 
   plugins: [react()],
-   build: {
+  build: {
     rollupOptions: {
-      input: '/src/utilities/main.jsx'  // <-- update this path to your new main.jsx location
+      input: '/src/utilities/main.jsx'
     }
   }
 })
